41d914f76cfb9e2b7c1f9e40657441e6905dae4b
Mauricio Moccelin <25235995+mauriciomoccelin@users.noreply.github.com>
2023-08-27 10:20:29 -0300

    feat: use split .json files to generate mock API's

--- src/database/app.repository.js
+++ src/database/app.repository.js
@@ -1,8 +1,21 @@
-const db = require("../mock.json");
+const fs = require("fs");
+const path = require("path");
+
+const jsonsInDir = fs
+  .readdirSync("src/mocks")
+  .filter((file) => path.extname(file) === ".json");
 
 class Repository {
   getApis() {
-    return db;
+    const apis = [];
+    jsonsInDir.forEach((file) => {
+      const fileData = fs.readFileSync(path.join("src/mocks", file));
+      const json = JSON.parse(fileData.toString());
+
+      apis.push(json);
+    });
+
+    return apis;
   }
 }
 
